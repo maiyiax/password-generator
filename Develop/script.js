b70f974dcd68d1eb12cc247c68a29f1e510f20d3
Mai Yia Xiong <maiyiax@gmail.com>
2020-09-20 16:38:57 -0500

    added prompts for selecting criteria

--- Develop/script.js
+++ Develop/script.js
@@ -4,11 +4,41 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Prompt user to select criterias
+function generatePassword() {
+
+  // confirm password length
+  var passwordLength = window.prompt(
+    "Please enter the number of characters you want for your new password. Password should contain more than 8 characters and no more than 128 characters"
+  );
+
+  // confirm lowercase
+  var lowerCase = window.confirm(
+    "Do you want lowecases in your password?"
+  );
+
+  // confirm uppercase
+  var upperCase = window.confirm(
+    "Do you want uppercases in your password?"
+  );
+
+  // confirm numeric
+  var numeric = window.confirm(
+    "Do you want numbers in your password?"
+  );
+
+  // confirm special characters
+  var scecialCharacter = window.confirm(
+    "Do you want special characters in your password?"
+  );
+
+};
+
 // Write password to the #password input
 function writePassword() {
 
   // add values to select from
-  var values = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~`!@#$%^&*()_-+={[}]|\:;<,>.?/~"
+  var values = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+<>ABCDEFGHIJKLMNOP123456789"
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
